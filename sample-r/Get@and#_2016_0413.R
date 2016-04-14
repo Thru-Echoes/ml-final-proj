86780a852c6800c928ede3b4a9c4b53726d06554
Oliver Muellerklein <omuellerklein@berkeley.edu>
2016-04-14 10:09:56 -0700

    Added unique-pos-occur + -users and modified Get@and#.R script

--- sample-r/Get@and#_2016_0413.R
+++ sample-r/Get@and#_2016_0413.R
@@ -19,18 +19,30 @@ PosTweet.OneString = paste(RawPosTweet, sep = " ", collapse = "")
 NegTweet.OneString = paste(RawNegTweet, sep = " ", collapse = "")
 #Collapse the vector RawPosTweet into one string, i.e., a character vector of legnth one. Separate using a space.
 
-posTest = str_extract_all(string = PosTweet.OneString, pattern = "@[[:alnum:]_]+")
-negTest = str_extract_all(string = NegTweet.OneString, pattern = "@[[:alnum:]_]+")
+#posTest = str_extract_all(string = PosTweet.OneString, pattern = "@[[:alnum:]_]+")
+#negTest = str_extract_all(string = NegTweet.OneString, pattern = "@[[:alnum:]_]+")
 #The list test contains all the extracted usernames (duplicate possible).
 
+posTest = str_extract_all(string = PosTweet.OneString, pattern = "(\\B@[[:alnum:]_]+)")
+negTest = str_extract_all(string = NegTweet.OneString, pattern = "(\\B@[[:alnum:]_]+)")
+
 negTest.vec = negTest[[1]] #Convert test into a character vector.
-UniqueUsers = unique(negTest.vec) #Find the unique usernames.
-n = length(UniqueUsers)
-UniqueOccurance = rep(NA, times = n)
+uniqueNegUsers = unique(negTest.vec) #Find the unique usernames.
+nNeg = length(uniqueNegUsers)
+uniqueNegOccur = rep(NA, times = nNeg)
+
+posTest.vec = posTest[[1]] #Convert test into a character vector.
+uniquePosUsers = unique(posTest.vec) #Find the unique usernames.
+nPos = length(uniquePosUsers)
+uniquePosOccur = rep(NA, times = nPos)
 #The vector UniqueOccurance will contain the frequency for each unique username.
 
-for (i in 1:n) {
-  UniqueOccurance[i] = sum(negTest.vec == UniqueUsers[i])
-  print(i / n) #Progress: done when it reaches 1.
+for (i in 1:nNeg) {
+  uniqueNegOccur[i] = sum(negTest.vec == uniqueNegUsers[i])
+  print(i / nNeg) #Progress: done when it reaches 1.
 }
 
+for (i in 1:nPos) {
+  uniquePosOccur[i] = sum(posTest.vec == uniquePosUsers[i])
+  print(i / nPos) #Progress: done when it reaches 1.
+}

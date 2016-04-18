ab1892f038e9c8c4110980532fb4c798a0888fd4
Paul Land <paul.land94@gmail.com>
2016-04-18 11:56:55 -0700

    Updated the data set to include length of tweet as a feature
    Added the number of characters of each tweet as a feature

--- data-mod-log/data.R
+++ data-mod-log/data.R
@@ -1,4 +1,4 @@
-library(Matrix)
+suppressMessages(library(Matrix))
 load("TrainTest.RData")
 Xsample <- X
 ysample <- y
@@ -8,3 +8,6 @@ vocab <- scan("vocab.txt", what="character", sep="\n", encoding="latin1")
 X <- readMM("X.mtx")
 y <- strtoi(scan("y.txt", what="integer", sep="\n", encoding="UTF-8"))
 raw.data <- read.csv("MaskedDataRawFixed.csv", header=TRUE)
+raw.data$SentimentText <- as.character(raw.data$SentimentText)
+tweet.lengths <- nchar(raw.data$SentimentText)
+X <- cbind(X, tweet.lengths)

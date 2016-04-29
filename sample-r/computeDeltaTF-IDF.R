c0120c5ddc8da13e41bb7f3086a8adf1e4730b2a
Oliver Muellerklein <omuellerklein@berkeley.edu>
2016-04-28 22:09:39 -0700

    Flipped signs of positive usernames and hashtag scores and ran xgb optimal param on 90 / 10 train / test

--- sample-r/computeDeltaTF-IDF.R
+++ sample-r/computeDeltaTF-IDF.R
@@ -1,7 +1,7 @@
 ##Load in a term frequency matrix (with sentiment scores as the first column).
 ##Compute the delta TF-IDF matrix.
 
-load("tf_164words_allRawData.rda")
+load("data/tf_164words_allRawData.rda")
 TFMatrix = tf
 
 posIndex = TFMatrix[, "Sentiment"] == 1
@@ -15,3 +15,19 @@ deltaIDF[is.nan(deltaIDF)] = 0
 
 deltaTF_IDF = t(apply(TFMatrix[, -1], 1, function(x) x * deltaIDF))
 save(deltaTF_IDF, file = "deltaTFIDF_164words_allRawData.rda")
+
+#### 
+
+load("data/deltaTF_IDF_AllFeatures.rda")
+load("data/deltaTF_IDF_ImpFeatures.rda")
+load("data/April26_wordFreq_relativePos_vs_neg_maxDiff.rda")
+
+relativeSentiDiff <- wordFreq.relative[, 6]
+
+sentiDiff_TF = t(apply(TFMatrix[, 2:119], 1, function(x) x * relativeSentiDiff))
+save(sentiDiff_TF, file = "data/relativeSentiDiff_164words.rda")
+
+# negative word (relative freq) has positive value 
+# positive word (relative freq) has negative value 
+
+
--- sample-r/train_test_boosting.R
+++ sample-r/train_test_boosting.R
@@ -13,21 +13,41 @@ parameters = list(max_depth = bestBoostPar["max.depth"],
                   lambda = bestBoostPar["lambda"], 
                   alpha = bestBoostPar["alpha"])
 
-Xtrain = deltaTF_IDF_ImpFeatures_labeled90
-ytrain = y_labeled90
+#Xtrain = deltaTF_IDF_ImpFeatures_labeled90
+#ytrain = y_labeled90
 
-Xtest = deltaTF_IDF_ImpFeatures_labeled10
-ytest = y_labeled10
+#Xtest = deltaTF_IDF_ImpFeatures_labeled10
+#ytest = y_labeled10
+
+set.seed(123)
+trainIndx <- sample(1:nrow(x24.labeled), size = floor(nrow(x24.labeled) * 0.9), replace = F)
+
+Xtrain <- x24.labeled[trainIndx, ]
+ytrain <- y.labeled[trainIndx]
+
+Xtest <- x24.labeled[-trainIndx, ]
+ytest <- y.labeled[-trainIndx]
 
 
-boostModel = xgboost(params = parameters, data = Xtrain, 
-                     label = ytrain, nrounds = bestBoostPar["idxminError"], verbose = 0, 
-                     nthred = 8)
+boostModel = xgboost(params = parameters, data = as.matrix(Xtrain), 
+                     label = ytrain, nrounds = bestBoostPar["idxminError"], verbose = 1, 
+                     nthred = 8, print.every.n = 10)
 
-yhat = predict(boostModel, Xtest)
-yhatPre = yhat > 0.5
+yhat = predict(boostModel, as.matrix(Xtest))
+yhatPre.5 = yhat > 0.5
 
-accuracy = sum(yhatPre == ytest) / length(ytest)
+yhatPre.median <- yhat > yhat.median 
 
-save(boostModel, yhat, yhatPre, accuracy, file = "bestBoostImpFeatures.rda")
+yhatPre.hi <- yhat > 0.51
+
+yhatPre.vHi <- yhat > 0.55
+
+accur.hi <- sum(yhatPre.hi == ytest) / length(ytest)
+accur.vHi <- sum(yhatPre.vHi == ytest) / length(ytest)
+accuracy.5 = sum(yhatPre.5 == ytest) / length(ytest)
+accuracy.median = sum(yhatPre.median == ytest) / length(ytest)
+
+save(boostModel, yhat, yhatPre.5, accuracy.5, file = "submission/April28_24features_xgb_5accur.rda")
+
+save(boostModel, yhat, yhatPre.median, accuracy.median, file = "submission/April28_24features_xgb_median_accur.rda")
 
